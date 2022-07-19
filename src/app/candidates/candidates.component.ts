2e14d603e9d64fd3c65650ab0f0e66cac43070c1
Kamil K <gkowalski.kamil@gmail.com>
2022-07-19 20:18:54 +0200

    Added a candidate list with styling, adjusted candidate editing portion

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidate } from './candidate';
+import { Candidates } from '../mock-candidates';
 
 @Component({
   selector: 'app-candidates',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidates.component.css']
 })
 export class CandidatesComponent implements OnInit {
-
+  title = 'Candidates';
+  candidates = Candidates;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedCandidate?: Candidate;
+  onSelect(candidate: Candidate): void{
+    this.selectedCandidate = candidate;
   }
 
 }
