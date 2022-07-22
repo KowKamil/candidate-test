fda60e38f800ecea11728221f5222a65e836f26e
Kamil K <gkowalski.kamil@gmail.com>
2022-07-22 14:36:51 +0200

    Added messages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CandidatesComponent } from './candidates/candidates.component';
 import { CandidateDetailComponent } from './candidate-detail/candidate-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, CandidatesComponent, CandidateDetailComponent],
+  declarations: [AppComponent, CandidatesComponent, CandidateDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
