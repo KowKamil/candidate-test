3a806d4dbaa50314a1fa75bd7ef986930f50f7dd
Kamil K <gkowalski.kamil@gmail.com>
2022-07-25 22:42:06 +0200

    introduced http client and in-memory api for testing it

--- src/app/candidate-service/candidate.service.ts
+++ src/app/candidate-service/candidate.service.ts
@@ -3,23 +3,44 @@ import { Candidate } from '../candidates/candidate';
 import { Candidates } from '../mock-candidates';
 import { Observable, of } from 'rxjs';
 import { MessageService } from '../message-service/message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CandidateService {
-  constructor(private messageService: MessageService) {}
+  private candidatesUrl = 'api/candidates';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
   getCandidates(): Observable<Candidate[]> {
-    const candidates = of(Candidates);
-    this.messageService.add('CandidateService: fetched candidates.');
-    return candidates;
+    return this.http.get<Candidate[]>(this.candidatesUrl).pipe(
+      tap((_) => this.log('fetched candidates')),
+      catchError(this.handleError<Candidate[]>('getCandidates', []))
+    );
   }
 
   getCandidate(id: number): Observable<Candidate> {
-    //no error handling yet
-    const candidate = Candidates.find((c) => c.id === id)!;
-    this.messageService.add(`CandidateService: fetched candidate id=${id}`);
-    return of(candidate);
+    const url = `${this.candidatesUrl}/${id}`;
+    return this.http.get<Candidate>(url).pipe(
+      tap((_) => this.log(`fetched candidate id=${id}`)),
+      catchError(this.handleError<Candidate>(`getCandidate id=${id}`))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.add(`CandidateService: ${message}`);
   }
 }
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Candidate } from './candidates/candidate';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const candidates: Candidate[] = [
+      { id: 10, firstName: 'Cotton', lastName: 'Mather' },
+      { id: 11, firstName: 'Deodat', lastName: 'Lawson' },
+      { id: 12, firstName: 'Edward', lastName: 'Bishop' },
+      { id: 13, firstName: 'Giles', lastName: 'Corey' },
+      { id: 14, firstName: 'James', lastName: 'Bayley' },
+      { id: 15, firstName: 'James', lastName: 'Russel' },
+      { id: 16, firstName: 'John', lastName: 'Hathorne' },
+      { id: 17, firstName: 'John', lastName: 'Proctor' },
+      { id: 18, firstName: 'John', lastName: 'Willard' },
+      { id: 19, firstName: 'Jonathan', lastName: 'Corwin' },
+      { id: 20, firstName: 'Samuel', lastName: 'Parris' },
+    ];
+    return { candidates };
+  }
+
+  // Overrides the genId method to ensure that a candidate always has an id.
+  // If the candidates array is empty,
+  // the method below returns the initial number (11).
+  // if the candidates array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(candidates: Candidate[]): number {
+    return candidates.length > 0
+      ? Math.max(...candidates.map((candidate) => candidate.id)) + 1
+      : 11;
+  }
+}
